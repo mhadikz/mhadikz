0a12044d984808cbe556f4eb80941530c63dc9bd
mhadikz <mhkaramzadeh7@gmail.com>
2020-11-09 20:18:39 +0330

    Initial codes

---
+++ myScripts.js
@@ -0,0 +1,134 @@
+$(function () {
+    $(".typed1").typed({
+        strings: [
+            "Hi"
+        ],
+        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+        stringsElement: null,
+        // typing speed
+        typeSpeed: 10,
+        // time before typing starts
+        startDelay: 500
+    });
+    $(".typed2").typed({
+        strings: [
+            ", I'm Mohammad Hadi Karamzadeh and an Android Developer."
+        ],
+        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+        stringsElement: null,
+        // typing speed
+        typeSpeed: 5,
+        // time before typing starts
+        startDelay: 1000
+    });
+    $(".typed3").typed({
+        strings: [
+            "If you would like to find me online follow the links below!"
+        ],
+        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+        stringsElement: null,
+        // typing speed
+        typeSpeed: 5,
+        // time before typing starts
+        startDelay: 4000
+    });
+    $(".typed4").typed({
+        strings: [
+            "Linkedin"
+        ],
+        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+        stringsElement: null,
+        // typing speed
+        typeSpeed: 5,
+        // time before typing starts
+        startDelay: 6500
+    });
+    $(".typed5").typed({
+        strings: [
+            "GitHub"
+        ],
+        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+        stringsElement: null,
+        // typing speed
+        typeSpeed: 5,
+        // time before typing starts
+        startDelay: 7500
+    });
+    $(".typed6").typed({
+        strings: [
+            "Stackoverflow"
+        ],
+        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+        stringsElement: null,
+        // typing speed
+        typeSpeed: 5,
+        // time before typing starts
+        startDelay: 8500
+    });
+    $(".typed7").typed({
+        strings: [
+            "Twitter"
+        ],
+        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+        stringsElement: null,
+        // typing speed
+        typeSpeed: 5,
+        // time before typing starts
+        startDelay: 9500
+    });
+    $(".typed8").typed({
+        strings: [
+            "Instagram"
+        ],
+        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+        stringsElement: null,
+        // typing speed
+        typeSpeed: 5,
+        // time before typing starts
+        startDelay: 10500
+    });
+    $(".typed9").typed({
+        strings: [
+            "mhkaramzadeh7@gmail.com"
+        ],
+        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+        stringsElement: null,
+        // typing speed
+        typeSpeed: 5,
+        // time before typing starts
+        startDelay: 11500
+    });
+    $(".typed10").typed({
+        strings: [
+            "Also, you can see my resume page here:"
+        ],
+        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+        stringsElement: null,
+        // typing speed
+        typeSpeed: 5,
+        // time before typing starts
+        startDelay: 12500
+    });
+    $(".typed11").typed({
+        strings: [
+            "Resume"
+        ],
+        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+        stringsElement: null,
+        // typing speed
+        typeSpeed: 5,
+        // time before typing starts
+        startDelay: 15000
+    });
+	 $(".typed12").typed({
+        strings: [
+            "I will be glad to contact me. :)"
+        ],
+        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+        stringsElement: null,
+        // typing speed
+        typeSpeed: 5,
+        // time before typing starts
+        startDelay: 16000
+    });
+});
